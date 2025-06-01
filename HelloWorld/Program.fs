0faf71b502d6502a703153a06d67eb3f7b5a5e76
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2025-06-02 00:58:00 +0200

    02-06-2025

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -1,70 +1,37 @@
 ﻿open System.IO
 
-type CommandLineInstruction<'a> = HelloWorld of (unit -> 'a) 
+type private CommandLineInstruction<'a> = HelloWorld of (unit -> 'a)
 
-type CommandLineProgram<'a> =
-    | Pure of 'a 
+type private CommandLineProgram<'a> =
+    | Pure of 'a
     | Free of CommandLineInstruction<CommandLineProgram<'a>>
 
-let private mapI f = function HelloWorld cont -> HelloWorld (fun () -> f (cont ()))   
+let private mapI f =
+    function
+    | HelloWorld cont 
+        -> HelloWorld <| fun () -> cont >> f <| ()
 
+//bind doesn't need to be tail-recursive or CPS, because it builds structure, not executes it
 let rec private bind f = 
     function
     | Free instr -> mapI (bind f) instr |> Free
     | Pure x     -> f x
 
-type CommandLineProgramBuilder = CommandLineProgramBuilder with
+type private CommandLineProgramBuilder = CommandLineProgramBuilder with
     member _.Bind(p, f) = bind f p
     member _.Return x = Pure x
     member _.ReturnFrom p = p
 
 let private cmdBuilder = CommandLineProgramBuilder
-       
-let rec interpret clp =
-    match clp with 
-    | Pure x                 -> 
-                              x
-    | Free (HelloWorld cont) ->
-                              printfn "Hello World!"
-                              interpret (cont ())
 
-cmdBuilder { return! Free (HelloWorld (fun () -> Pure ())) } |> interpret
+let rec private interpretCPS (cont: 'a -> 'b) (clp: CommandLineProgram<'a>) : 'b =
+    match clp with
+    | Pure x 
+        ->
+        cont x
+    | Free (HelloWorld next) 
+        ->
+        printfn "Hello World!"
+        interpretCPS <| cont <| next ()
 
-
-
-    //Malbolge
-    //(=<`#9]~6ZY32Vx/4Rs+0No-&Jk)"Fh}|Bcy?`=*z]Kw%oG4UUS0/@-ejc(:'8dc.
-
-    //Haskell "Hello, World!" development
-    //First..
-    //main :: IO ()
-    //main = putStrLn "Hello, World!" >>= \_ -> return ()
-
-    //Then...
-    //main :: IO ()
-    //main = putStrLn "Hello, World!" >> return ()
-
-    //Then ...
-    //main :: IO ()
-    //main = do
-       // putStrLn "Hello, World!"
-
-    //...and now
-    //main :: IO ()
-   // main = putStrLn "Hello, World!"
-
-
-    //**********************
-
-    //F# CE
-
-    //let private (>>=) x f = f x 
-
-    //type IO = IO with    
-        //member _.Bind(x, f) = (>>=) x f
-       // member _.Return x = x
-
-    //let testingIO () = 
-        //IO { return! printfn "Hello, World!" }
-
-
+cmdBuilder { return! Free (HelloWorld (fun () -> Pure ())) } |> interpretCPS id
